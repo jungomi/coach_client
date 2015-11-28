b173bf498522e66fcc989ab263ec16e945fd364b
Michael Jungo <michael.jungo@unifr.ch>
2015-11-29 00:42:39 +0100

    add webmock and vcr for testing

--- spec/coach_client_spec.rb
+++ spec/coach_client_spec.rb
@@ -4,8 +4,4 @@ describe CoachClient do
   it 'has a version number' do
     expect(CoachClient::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coach_client'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
