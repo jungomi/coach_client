bd42cc9a8b12e4a2bb2eb3cbf08ef33833192b70
Michael Jungo <michael.jungo@unifr.ch>
2015-11-29 02:44:00 +0100

    improve error handling in request

--- lib/coach_client/client.rb
+++ lib/coach_client/client.rb
@@ -11,55 +11,35 @@ module CoachClient
         CoachClient::Request.get("#{@url}authenticateduser/",
                                  username: username, password: password)
         true
-      rescue RestClient::Exception
+      rescue CoachClient::Unauthorized
         false
       end
     end
 
     def get_sport(sportname)
       sport = CoachClient::Sport.new(self, sportname)
-      begin
-        sport.update
-      rescue RestClient::Exception
-        raise CoachClient::NotFound, "Sport not found"
-      end
+      sport.update
     end
 
     def get_user(username)
       user = CoachClient::User.new(self, username)
-      begin
-        user.update
-      rescue RestClient::Exception
-        raise CoachClient::NotFound, "User not found"
-      end
+      user.update
     end
 
     def get_partnership(user1, user2)
       partnership = CoachClient::Partnership.new(self, user1, user2)
-      begin
-        partnership.update
-      rescue RestClient::Exception
-        raise CoachClient::NotFound, "Partnership not found"
-      end
+      partnership.update
     end
 
     def get_user_subscription(user, sport)
       subscription = CoachClient::UserSubscription.new(self, user, sport)
-      begin
-        subscription.update
-      rescue RestClient::Exception
-        raise CoachClient::NotFound, "Subscription not found"
-      end
+      subscription.update
     end
 
     def get_partnership_subscription(user1, user2, sport)
       partnership = CoachClient::Partnership.new(self, user1, user2)
       subscription = CoachClient::PartnershipSubscription.new(self, partnership, sport)
-      begin
-        subscription.update
-      rescue RestClient::Exception
-        raise CoachClient::NotFound, "Subscription not found"
-      end
+      subscription.update
     end
   end
 end
--- lib/coach_client/request.rb
+++ lib/coach_client/request.rb
@@ -4,36 +4,62 @@ module CoachClient
 
     def self.get(url, username: nil, password: nil, **header)
       header.merge!(DEFAULT_HEADER)
-      response = RestClient::Request.execute(method: :get, url: url,
-                                             user: username,
-                                             password: password,
-                                             headers: header)
+      begin
+        response = RestClient::Request.execute(method: :get, url: url,
+                                               user: username,
+                                               password: password,
+                                               headers: header)
+      rescue RestClient::ResourceNotFound => e
+        raise CoachClient::NotFound, e.message
+      rescue RestClient::Unauthorized => e
+        raise CoachClient::Unauthorized, e.message
+      end
       Response.new(response.headers, response, response.code)
     end
 
     def self.put(url, username: nil, password: nil, payload:, **header)
       header.merge!(DEFAULT_HEADER)
-      response = RestClient::Request.execute(method: :put, url: url,
-                                             user: username,
-                                             password: password,
-                                             payload: payload, headers: header)
+      begin
+        response = RestClient::Request.execute(method: :put, url: url,
+                                               user: username,
+                                               password: password,
+                                               payload: payload,
+                                               headers: header)
+      rescue RestClient::ResourceNotFound => e
+        raise CoachClient::NotFound, e.message
+      rescue RestClient::Unauthorized => e
+        raise CoachClient::Unauthorized, e.message
+      end
       Response.new(response.headers, response, response.code)
     end
 
     def self.post(url, username: nil, password: nil, payload:, **header)
       header.merge!(DEFAULT_HEADER)
-      response = RestClient::Request.execute(method: :post, url: url,
-                                             user: username,
-                                             password: password,
-                                             payload: payload, headers: header)
+      begin
+        response = RestClient::Request.execute(method: :post, url: url,
+                                               user: username,
+                                               password: password,
+                                               payload: payload,
+                                               headers: header)
+      rescue RestClient::ResourceNotFound => e
+        raise CoachClient::NotFound, e.message
+      rescue RestClient::Unauthorized => e
+        raise CoachClient::Unauthorized, e.message
+      end
       Response.new(response.headers, response, response.code)
     end
 
     def self.delete(url, username: nil, password: nil, **header)
-      response = RestClient::Request.execute(method: :delete, url: url,
-                                             user: username,
-                                             password: password,
-                                             headers: header)
+      begin
+        response = RestClient::Request.execute(method: :delete, url: url,
+                                               user: username,
+                                               password: password,
+                                               headers: header)
+      rescue RestClient::ResourceNotFound => e
+        raise CoachClient::NotFound, e.message
+      rescue RestClient::Unauthorized => e
+        raise CoachClient::Unauthorized, e.message
+      end
       Response.new(response.headers, response, response.code)
     end
   end
